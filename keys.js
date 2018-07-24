d84fd647fd1d332b87cce3d4f20d760bc9d1b513
Zekarias Solmoon <36565439+Zsolomon1@users.noreply.github.com>
2018-07-24 14:17:19 -0400

    Add files via upload

---
+++ keys.js
@@ -0,0 +1,13 @@
+// console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'uzgVFDvtm3NmKTpAK2dzkc2yS',
+  consumer_secret: 'PHdkZaJ8iO966VUgYHxw3Kbb3uNSgBMaRVHkLUccL6J1nLG5oW',
+  access_token_key: '634524575-mHVwyZ9ysuDoNJQjLrhDnsLdy7VhjdV8ayNlBPYN',
+  access_token_secret: '8f3W6dAm8Fnu8Rjf2TWFMpNyex3dVKT0ZQOpaqPWB',
+}
+
+exports.spotifyKeys = {
+	id: '4ccfd6c00986418691afc2e50590c29e',
+	secret: '0e20675d70a443de8731ccf220344647',
+}
